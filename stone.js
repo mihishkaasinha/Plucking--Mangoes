c4f0adf027c468b9023e0d5e19ae970c72ee1ac1
mihishkaasinha <72974069+mihishkaasinha@users.noreply.github.com>
2021-01-05 11:41:08 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -10,7 +10,7 @@ class Stone
         this.body = Bodies.rectangle(x, y, w, h, options);
         this.w = w;
         this.h = h;
-        this.image = loadImage("images/stone.png");
+        this.image = loadImage("stone.png");
         this.image.scale = 0.11
         World.add(world, this.body);
     }
